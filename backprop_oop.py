a2caec86c40397683ddf65290e3fc821115538d6
Andrea Cognolato <andrecogno@hotmail.it>
2020-05-07 21:53:55 +0200

    Show partial derivatives on edges, add .gitignore

--- backprop_oop.py
+++ backprop_oop.py
@@ -62,14 +62,13 @@ class NullaryOp(Op):
     def node_repr(self):
         return (f"<<font color=\"blue\">{self.id}</font><br/>"
                 f"{self.__class__.__name__}<br/>"
-                f"fwd: {self.fwd_val:.2f}<br/>"
-                f"adj: {self.adjoint:.2f}>")
+                f"fwd: {self.fwd_val:.2f}<br/>>")
 
     def backward_local_grad(self):
         pass
 
-    def backward(self, start_value):
-        self.adjoint += start_value
+    def backward(self, parent_adjoint):
+        self.adjoint += parent_adjoint
 
 
 class UnaryOp(Op):
@@ -85,14 +84,16 @@ class UnaryOp(Op):
     def graph(self, G):
         G.node(
             self.id, self.node_repr())
-        G.edge(self.id, self.arg.id)
+        G.edge(self.id, self.arg.id, self.edge_repr())
         self.arg.graph(G)
 
     def node_repr(self):
         return (f"<<font color=\"blue\">{self.id}</font><br/>"
                 f"{self.__class__.__name__}<br/>"
-                f"fwd: {self.fwd_val:.2f}<br/>"
-                f"\u2202{self.id}/\u2202{self.arg.id}: {self.darg:.2f}>")
+                f"fwd: {self.fwd_val:.2f}<br/>>")
+
+    def edge_repr(self):
+        return(f"\u2202{self.id}/\u2202{self.arg.id}: {self.darg:.2f}")
 
 
 class BinaryOp(Op):
@@ -111,29 +112,36 @@ class BinaryOp(Op):
     def graph(self, G):
         G.node(
             self.id, self.node_repr())
-        G.edge(self.id, self.lhs.id)
-        G.edge(self.id, self.rhs.id)
+        G.edge(self.id, self.lhs.id, self.left_edge_repr())
+        G.edge(self.id, self.rhs.id, self.right_edge_repr())
         self.rhs.graph(G)
         self.lhs.graph(G)
 
     def node_repr(self):
         return (f"<<font color=\"blue\">{self.id}</font><br/>"
                 f"{self.__class__.__name__}<br/>"
-                f"fwd: {self.fwd_val:.2f}<br/>"
-                f"\u2202{self.id}/\u2202{self.lhs.id}: {self.dlhs:.2f}<br/>"
-                f"\u2202{self.id}/\u2202{self.rhs.id}: {self.drhs:.2f}>")
+                f"fwd: {self.fwd_val:.2f}<br/>>")
 
-    def backward(self, start_value):
-        self.dlhs *= start_value
-        self.drhs *= start_value
-        self.lhs.backward(self.dlhs)
-        self.rhs.backward(self.drhs)
+    def right_edge_repr(self):
+        return f"<\u2202{self.id}/\u2202{self.rhs.id}: {self.drhs:.2f}>"
+
+    def left_edge_repr(self):
+        return f"<\u2202{self.id}/\u2202{self.lhs.id}: {self.dlhs:.2f}>"
+
+    def backward(self, parent_adjoint):
+        self.lhs.backward(self.dlhs * parent_adjoint)
+        self.rhs.backward(self.drhs * parent_adjoint)
 
 
 class Number(NullaryOp):
     def __init__(self, value):
         super().__init__()
         self.value = value
+
+    def node_repr(self):
+        return (f"<<font color=\"blue\">{self.id}</font><br/>"
+                f"{self.__class__.__name__}: {self.value}<br/>"
+                f"fwd: {self.fwd_val:.2f}<br/>>")
 
     def forward(self, env):
         self.fwd_val = self.value
@@ -144,6 +152,11 @@ class Var(NullaryOp):
     def __init__(self, name):
         super().__init__()
         self.name = name
+
+    def node_repr(self):
+        return (f"<<font color=\"blue\">{self.id}</font><br/>"
+                f"{self.__class__.__name__}: {self.name}<br/>"
+                f"fwd: {self.fwd_val:.2f}<br/>>")
 
     def forward(self, env):
         self.fwd_val = env[self.name]
@@ -162,9 +175,8 @@ class ExpOp(UnaryOp):
         self.darg = math.exp(self.arg.fwd_val)
         self.arg.backward_local_grad()
 
-    def backward(self, start_value):
-        self.darg *= start_value
-        self.arg.backward(self.darg)
+    def backward(self, parent_adjoint):
+        self.arg.backward(self.darg * parent_adjoint)
 
 
 def exp(arg):
@@ -201,13 +213,22 @@ class AddOp(BinaryOp):
         self.rhs.backward_local_grad()
 
 
+# Define the expression
 x1 = Var("x1")
 x2 = Var("x2")
 z = exp(x1*x2 + Number(5)*x1)
 
+# Run the computation, saving the computed values at each node.
 assert (abs(z.forward({"x1": 0.2, "x2": 0.3}) - math.exp(1.06)) <= 5*1e-15)
 
+# Traverse the computation DAG backwards, calculating the adjoint at each edge.
+# NOTE(Andrea): this could be done in the forward pass but, in general, not in
+# the initialization (e.g. to make the adjoint for the multiplication we need
+# to know the terms which are being multiplied).
 z.backward_local_grad()
+
+# Traverse the computation DAG backwards, at each edge multiplying the parent's
+# adjoint with the edge's adjoint.
 z.backward(1)
 
 G = make_graph()
